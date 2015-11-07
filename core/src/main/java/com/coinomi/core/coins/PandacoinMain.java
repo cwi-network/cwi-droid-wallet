0c196f4ccb84cd128754ac1c6ddb83a78676cfaa
Felix Ramon <felix.fire619@gmail.com>
2015-11-06 19:10:55 -0800

    Pandacoin Fix & Alpha 5 APK

--- core/src/main/java/com/coinomi/core/coins/PandacoinMain.java
+++ core/src/main/java/com/coinomi/core/coins/PandacoinMain.java
@@ -1,6 +1,6 @@
 package com.coinomi.core.coins;
 
-import com.coinomi.core.coins.families.PeerFamily;
+import com.coinomi.core.coins.families.BitFamily;
 
 /**
  * @author John L. Jegutanis
@@ -11,23 +11,25 @@ public class PandacoinMain extends CoinType {
 
         addressHeader = 55;
         p2shHeader = 22;
-        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
-        spendableCoinbaseDepth = 500;
+        acceptableAddressCodes = new int[]{addressHeader, p2shHeader};
+        spendableCoinbaseDepth = 100;
+        dumpedPrivateKeyHeader = 128;
 
-        family = PeerFamily.get();
+        family = BitFamily.get();
         name = "Pandacoin";
         symbol = "PND";
-        uriScheme = "pandacoin"; // TODO verify, could be ppcoin?
-        bip44Index = 42;
-        unitExponent = 6;
-        feePerKb = value(10000); // 0.01PPC, careful Peercoin has 1000000 units per coin
-        minNonDust = value(10000); // 0.01PPC
-        softDustLimit = minNonDust;
-        softDustPolicy = SoftDustPolicy.NO_POLICY;
-        signedMessageHeader = toBytes("PandaCoin Signed Message:\n");
+        uriScheme = "pandacoin";
+        bip44Index = 37;
+        unitExponent = 8;
+        feePerKb = value(10000);
+        minNonDust = value(5460);
+        softDustLimit = value(1000000); // 0.01 BTC
+        softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
+        signedMessageHeader = toBytes("Pandacoin Signed Message:\n");
     }
 
     private static PandacoinMain instance = new PandacoinMain();
+
     public static synchronized PandacoinMain get() {
         return instance;
     }
